f78112eac5ca7d74bb4bb3eb1c11fa1b81b3c16c
Makar <zetikspb@yandex.ru>
2026-01-29 05:19:10 +0300

    Simplify App.tsx - remove duplicate fetchMe query, let Account handle auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,10 @@
 import { Account } from "./Account/Account";
-import { fetchMe } from "./api/User";
 import "./App.css";
-import { UserView } from "./components/UserView";
-import { useQuery } from "@tanstack/react-query";
-
-interface User {
-  id: string;
-  username: string;
-  email: string;
-}
 
 function App() {
-  const { data: user, isLoading } = useQuery<User>({
-    queryKey: ['users', 'me'],
-    queryFn: fetchMe,
-    retry: false,
-    refetchOnWindowFocus: false
-  });
-
-  if (isLoading) {
-    return <div>Загрузка...</div>;
-  }
-
   return (
     <div className="app">
       <div className="container">
-        {user && <UserView user={user} />}
         <Account />
       </div>
     </div>
